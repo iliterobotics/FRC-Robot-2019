88662e2ed3142246f3728685af4de0ce82f3cf0d
Stephen Welch <stephenwelchva@gmail.com>
2018-10-23 14:14:00 -0400

    Finish up architecture, add documentation

--- robot/src/main/java/us/ilite/robot/hardware/IHardware.java
+++ robot/src/main/java/us/ilite/robot/hardware/IHardware.java
@@ -1,5 +1,12 @@
 package us.ilite.robot.hardware;
 
+/**
+ * Instead of a central Hardware class, like we used last year, we're separating hardware by Module.
+ * The idea behind this is threefold:
+ * 1. Reduce the size (and constructor boilerplate) and complexity of Modules,
+ * 2. Maintain something approaching best practices while still using dependency injection,
+ * 3. Provide a high level way to simulate robot hardware (i.e. NOT making passthrough classes for every part of WPILib/CTRE)
+ */
 public interface IHardware {
 
     void init();
