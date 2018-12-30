40448d7bc16a3297745e4d9094dc6cf71062ec7f
Stephen Welch <stephenwelchva@gmail.com>
2018-12-29 23:54:13 -0500

    Document loop

--- robot/src/main/java/us/ilite/robot/loops/Loop.java
+++ robot/src/main/java/us/ilite/robot/loops/Loop.java
@@ -2,6 +2,9 @@ package us.ilite.robot.loops;
 
 import us.ilite.robot.modules.Module;
 
+/**
+ * An extension of a Module that runs a separate update method called at high frequency.
+ */
 public abstract class Loop extends Module {
 
     public abstract void loop(double pNow);
