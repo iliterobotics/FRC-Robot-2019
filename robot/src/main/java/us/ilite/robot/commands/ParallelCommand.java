9982ff6864dec548a95c8eb1f2320c66e7a8c1c8
Stephen Welch <stephenwelchva@gmail.com>
2018-12-29 23:53:20 -0500

    Document parallel command

--- robot/src/main/java/us/ilite/robot/commands/ParallelCommand.java
+++ robot/src/main/java/us/ilite/robot/commands/ParallelCommand.java
@@ -5,6 +5,12 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Updates a given list of commands in parallel, rather than one at a time.
+ * ParallelCommand is a command itself, so it can be used in sequence with other commands if
+ * needed. Some potential use cases are: turning and raising an elevator at the same time,
+ * Grabbing vision targeting data and turning to a heading based on that data, etc.
+ */
 public class ParallelCommand implements ICommand {
 
   List<ICommand> mCommandList;
