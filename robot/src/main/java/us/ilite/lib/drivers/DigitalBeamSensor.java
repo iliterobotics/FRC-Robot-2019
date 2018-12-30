5c3fd8aa86f541cc79d2fb9c169b3d982b5ebbea
Stephen Welch <stephenwelchva@gmail.com>
2018-12-29 20:15:06 -0500

    Allow user to define a "broken" state for beam break and document

--- robot/src/main/java/us/ilite/lib/drivers/DigitalBeamSensor.java
+++ robot/src/main/java/us/ilite/lib/drivers/DigitalBeamSensor.java
@@ -2,18 +2,41 @@ package us.ilite.lib.drivers;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 
+/**
+ * Wrapper class for a beam-break sensor.
+ */
 public class DigitalBeamSensor  {
 
 	private final DigitalInput mBeamInput;
+	private final boolean mBeamBrokenState; //  Whether the sensor normally returns "true" or "false" when the beam is broken.
 	
-	public DigitalBeamSensor(int pInputChannel) {
+	public DigitalBeamSensor(int pInputChannel, boolean pBeamBrokenState) {
 		mBeamInput = new DigitalInput(pInputChannel);
+		mBeamBrokenState = pBeamBrokenState;
 	}
+
+	public DigitalBeamSensor(int pInputChannel) {
+		this(pInputChannel, true);
+	}
+
+	/**
+	 *
+	 * @return Returns true if the beam is broken.
+	 */
 	public boolean isBroken() {
 	  // NOTE - if the beam is noisy, we can do some filtered average based upon the leading
 	  // edge of the detection rather than just a 'get'.  This effectively debounces the signal.
 	  //	  mBeamInput.readRisingTimestamp()
-		return mBeamInput.get();
+		boolean reading =  mBeamInput.get();
+
+		if(reading == true && mBeamBrokenState == true) {
+			return true;
+		} else if(reading == false && mBeamBrokenState == false) {
+			return false;
+		} else {
+			return true;
+		}
+
 	}
 	
 }
