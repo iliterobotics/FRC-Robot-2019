c6fa28682088c06bd7de5fd3a4af46acbafaefbc
Stephen Welch <stephenwelchva@gmail.com>
2019-01-03 20:26:50 -0500

    Remove simulation display

--- display/src/main/java/us/ilite/display/simulation/ISimulationListener.java
+++
@@ -1,9 +0,0 @@
-package us.ilite.display.simulation;
-
-import us.ilite.common.lib.geometry.Pose2d;
-
-public interface ISimulationListener {
-
-    void update(double pTimeStamp, Pose2d pCurrentPose);
-
-}
