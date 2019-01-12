35c98a2d5ca5551b67855873fe867c0e1fb2728c
Unknown <1natechong@gmail.com>
2019-01-12 13:27:15 -0500

    Revert "Merge remote-tracking branch 'origin/feature/JavaFXAuton' into feature/JavaFXAuton"
    This reverts commit 4f761dd335085f8bac3db84e1c09aa48e7b1060e.

--- common/src/main/java/us/ilite/common/types/auton/EStartingPosition.java
+++ common/src/main/java/us/ilite/common/types/auton/EStartingPosition.java
@@ -1,14 +1,1 @@
 package us.ilite.common.types.auton;
-
-public enum EStartingPosition {
-    MIDDLE_LEFT,
-	MIDDLE_RIGHT,
-    BOTTOM_LEFT,
-    BOTTOM_RIGHT,
-    UNKNOWN;
-  public static EStartingPosition intToEnum(int num) {
-    if(num == -1)return UNKNOWN;
-    if(num >= values().length || num < 0) return UNKNOWN;
-    return values()[num];
-  }
-}
