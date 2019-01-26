4fb59205e71fa8c160043ab5047ff238e6d88c52
Chris O'Connell <coconnell84@gmail.com>
2019-01-26 10:53:06 -0500

    Initial commit of a unit test for the MotionToAbsoluteDistanceCommand

---
+++ robot/src/test/java/us/ilite/robot/commands/MotionToAbsoluteDistanceCommandTest.java
@@ -0,0 +1,42 @@
+package us.ilite.robot.commands;
+
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentMatcher;
+
+import us.ilite.robot.modules.Drive;
+import us.ilite.robot.modules.DriveMessage;
+
+public class MotionToAbsoluteDistanceCommandTest {
+
+    private MotionToAbsoluteDistanceCommand distanceToCommand;
+    private Drive drive;
+    private IAbsoluteDistanceProvider absoluteDistanceProvider;
+    private final double DESIRED_DISTANCE_IN_INCHES = 25;
+
+    @Before
+    public void init() {
+        drive = mock(Drive.class);
+        absoluteDistanceProvider = mock(IAbsoluteDistanceProvider.class);
+        distanceToCommand = new MotionToAbsoluteDistanceCommand(drive, absoluteDistanceProvider,DESIRED_DISTANCE_IN_INCHES);
+    }
+    @Test
+    public void testSensorReadingGreater() {
+        when(absoluteDistanceProvider.getAbsoluteDistanceInInches()).thenReturn(DESIRED_DISTANCE_IN_INCHES * 2);
+
+        boolean isDone = distanceToCommand.update(System.currentTimeMillis());
+
+        assertFalse(isDone);
+        verify(drive, times(1)).setDriveMessage(argThat(getArgMatcher(-1, -1)));
+
+    }
+
+    private static ArgumentMatcher<DriveMessage> getArgMatcher(double leftOutput, double rightOutput) {
+        return (driverMessage)->{
+            return driverMessage.leftOutput == leftOutput && driverMessage.rightOutput == rightOutput;
+        };
+    }
+}
