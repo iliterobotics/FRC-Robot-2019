cd7e87f99a87bf2e79e1b7ae0fb3084c8b00ef7c
Chris O'Connell <coconnell84@gmail.com>
2019-01-26 09:06:36 -0500

    Moved the instantiation of the NetworkTableInstance
    The instance is now being instantiated when it's used so that code
    that doesn't want it, such as unit test doesn't

--- common/src/main/java/us/ilite/common/lib/util/NetworkTablesConstantsBase.java
+++ common/src/main/java/us/ilite/common/lib/util/NetworkTablesConstantsBase.java
@@ -14,34 +14,30 @@ import com.flybotix.hfr.util.log.Logger;
 import com.google.gson.Gson;
 import com.google.gson.JsonParseException;
 
-import edu.wpi.first.networktables.EntryListenerFlags;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import us.ilite.common.config.SystemSettings;
 
 /**
  * Add your docs here.
  */
 public abstract class NetworkTablesConstantsBase {
 
-    private static final NetworkTableInstance kNetworkTableInstance = NetworkTableInstance.getDefault();
-
     private final ILog mLog = Logger.createLog(NetworkTablesConstantsBase.class);
-    private final NetworkTable mTable;
     private final Field[] mDeclaredFields;
     private final Gson mGson;
 
     public NetworkTablesConstantsBase() {
-        mTable = kNetworkTableInstance.getTable(this.getClass().getSimpleName().toUpperCase());
+        
         mDeclaredFields = this.getClass().getDeclaredFields();
         mGson = new Gson();
     }
 
     public void writeToNetworkTables() {
+        NetworkTable table = NetworkTableInstance.getDefault().getTable(this.getClass().getSimpleName().toUpperCase());
         for (Field f : mDeclaredFields) {
             if (java.lang.reflect.Modifier.isStatic(f.getModifiers())) {
-                NetworkTableEntry entry = mTable.getEntry(f.getName());
+                NetworkTableEntry entry = table.getEntry(f.getName());
 
                 try {
                     entry.setString(mGson.toJson(f.get(this)));
@@ -57,8 +53,9 @@ public abstract class NetworkTablesConstantsBase {
     }
 
     public void loadFromNetworkTables() {
+        NetworkTable table = NetworkTableInstance.getDefault().getTable(this.getClass().getSimpleName().toUpperCase());
         for (Field f : mDeclaredFields) {
-            NetworkTableEntry entry = mTable.getEntry(f.getName());
+            NetworkTableEntry entry = table.getEntry(f.getName());
             if(entry.exists()) {
                 try {
                     f.set(this, mGson.fromJson(entry.getString(""), f.getGenericType()));
