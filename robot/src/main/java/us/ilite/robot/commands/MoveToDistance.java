f9a3e4671b365bd7831bd1764cd57fd33fd7954d
Stephen Welch <stephenwelchva@gmail.com>
2019-01-27 16:19:24 -0500

    Add setters for power and allowable error

--- robot/src/main/java/us/ilite/robot/commands/MoveToDistance.java
+++ robot/src/main/java/us/ilite/robot/commands/MoveToDistance.java
@@ -5,6 +5,8 @@ import us.ilite.robot.modules.Drive;
 import us.ilite.robot.modules.DriveMessage;
 
 public class MoveToDistance implements ICommand {
+    private double mEpsilonInches = 1.0;
+    private double mPower = 0.5;
     private final Drive drive;
     private final IAbsoluteDistanceProvider distanceProvider;
     private final double goalDistanceInInches;
@@ -29,20 +31,30 @@ public class MoveToDistance implements ICommand {
         boolean isFinished = false;
         //Take a measurment of how far the distance provider
         double deltaDistance = distanceProvider.getAbsoluteDistanceInInches() - goalDistanceInInches;
-        if(Util.epsilonEquals(deltaDistance, 0.0, 0.001)) {
+        if(Util.epsilonEquals(deltaDistance, 0.0, mEpsilonInches)) {
             isFinished = true;
         } else if(deltaDistance > 0.0d) {
             //Move backwards
-            drive.setDriveMessage(DriveMessage.fromThrottleAndTurn(1.0, 0));
+            drive.setDriveMessage(DriveMessage.fromThrottleAndTurn(mPower, 0));
             isFinished = false;
         } else if(deltaDistance < 0.0d) {
-            drive.setDriveMessage(DriveMessage.fromThrottleAndTurn(-1.0, 0));
+            drive.setDriveMessage(DriveMessage.fromThrottleAndTurn(-mPower, 0));
         } else {
             //Should be impossible, but we'll say we're done
             isFinished = true;
         }
         
         return isFinished;
+    }
+
+    public MoveToDistance setEpsilonInches(double pEpsilonInches) {
+        mEpsilonInches = pEpsilonInches;
+        return this;
+    }
+
+    public MoveToDistance setPower(double pPower) {
+        mPower = pPower;
+        return this;
     }
 
 }
