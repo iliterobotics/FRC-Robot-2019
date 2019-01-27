d64b7eadd946b2b55555d4d0e1ca306b8a2df9a0
Chris O'Connell <coconnell84@gmail.com>
2019-01-27 12:47:23 -0500

    Added javadoc to the unit test

--- robot/src/test/java/us/ilite/robot/os/commands/GetLocalIPTest.java
+++ robot/src/test/java/us/ilite/robot/os/commands/GetLocalIPTest.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package us.ilite.robot.os.commands;
 
 import static org.junit.Assert.*;
@@ -12,27 +5,38 @@ import static org.mockito.Mockito.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.Optional;
 
 import org.junit.Test;
 
 /**
- * Add your docs here.
+ * Unit test class to test {@link GetLocalIP}
  */
 public class GetLocalIPTest {
 
+    /**
+     * Method to test the {@link GetLocalIP#getIPFromInputStream(BufferedReader)}. 
+     * 
+     * Parameter: Null
+     * Expected behavior: The method should return an empty optional indiciating that 
+     * no IP address could be found
+     */
     @Test
     public void testNullReader() {
         Optional<String> returnVal = GetLocalIP.getIPFromInputStream(null);
         assertNotNull(returnVal);
         assertTrue(returnVal.isEmpty());
     }
-    @Test
-    public void testEmptyReader() {
-        Optional<String> returnVal = GetLocalIP.getIPFromInputStream(null);
-        assertNotNull(returnVal);
-        assertTrue(returnVal.isEmpty());
-    }
+
+    /**
+     * Method to test the {@link GetLocalIP#getIPFromInputStream(BufferedReader)}. 
+     * 
+     * Parameter: mocked buffered reader that will throw an IllegalArgumentException when the 
+     * readLine method is called. 
+     * Expected behavior: The method should return an empty optional indiciating that 
+     * no IP address could be found
+     */
     @Test
     public void testReaderIOException() throws IOException{
         BufferedReader br = mock(BufferedReader.class);
@@ -41,6 +45,15 @@ public class GetLocalIPTest {
         assertNotNull(returnVal);
         assertTrue(returnVal.isEmpty());
     }
+    /**
+     * Method to test the {@link GetLocalIP#getIPFromInputStream(BufferedReader)}. 
+     * 
+     * Parameter: mocked BufferedReader that will first return a string with no IP 
+     * and then a null string so that the method reading the buffered reader does not 
+     * get stuck in an infinite loop. 
+     * Expected behavior: The method should return an empty optional indiciating that 
+     * no IP address could be found
+     */
     @Test
     public void testReaderNoIPs() throws IOException{
         BufferedReader br = mock(BufferedReader.class);
@@ -51,6 +64,14 @@ public class GetLocalIPTest {
         assertTrue(returnVal.isEmpty());
     }
 
+    /**
+     * Method to test the {@link GetLocalIP#getIPFromInputStream(BufferedReader)}. 
+     * 
+     * Parameter: mocked bufferreader that will first return a valid IP address the first
+     * time and will return the same IP address in the next iteration. The final iteration will 
+     * return a null so that the buffered reader reading logic does not get stuck in an infinite loops
+     * Expected behavior: The method should return a single IP that matches the value. 
+     */
     @Test
     public void testReaderMultipleIpsSame() throws IOException{
         BufferedReader br = mock(BufferedReader.class);
@@ -59,5 +80,25 @@ public class GetLocalIPTest {
         Optional<String> returnVal = GetLocalIP.getIPFromInputStream(br);
         assertNotNull(returnVal);
         assertFalse(returnVal.isEmpty());
+        assertEquals("10.18.85.186", returnVal.get());
+    }
+
+    /**
+     * Method to test the {@link GetLocalIP#getIPFromInputStream(BufferedReader)}. 
+     * 
+     * Parameter: mocked bufferreader that will first return a valid IP address the first
+     * time and will return a different IP address in the next iteration. The final iteration will 
+     * return a null so that the buffered reader reading logic does not get stuck in an infinite loops
+     * Expected behavior: The method should return a single IP that matches the first IP
+     **/
+    @Test
+    public void testReaderMultipleIpsDifferent() throws IOException{
+        BufferedReader br = mock(BufferedReader.class);
+        //We want to return a bogus string first and then null so there is not an infinite loop
+        when(br.readLine()).thenReturn("10.18.85.186","10.18.85.187",(String)null);
+        Optional<String> returnVal = GetLocalIP.getIPFromInputStream(br);
+        assertNotNull(returnVal);
+        assertFalse(returnVal.isEmpty());
+        assertEquals("10.18.85.186", returnVal.get());
     }
 }
