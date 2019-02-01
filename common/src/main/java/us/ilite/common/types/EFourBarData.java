44577a85b5b3deab4a9315e69118f64a694e165e
John Anselmo <jjohnanselmo@gmail.com>
2019-02-01 16:07:07 -0500

    Formatting!

--- common/src/main/java/us/ilite/common/types/EFourBarData.java
+++ common/src/main/java/us/ilite/common/types/EFourBarData.java
@@ -3,10 +3,7 @@ package us.ilite.common.types;
 import com.flybotix.hfr.codex.CodexOf;
 
 public enum EFourBarData implements CodexOf<Double>{
-    STATE,
-    A_OUTPUT,
-    B_OUTPUT,
-    A_VOLTAGE,
-    B_VOLTAGE,
-    ANGLE;
+    STATE, ANGLE,
+    A_OUTPUT, B_OUTPUT,
+    A_VOLTAGE, B_VOLTAGE;
 }
