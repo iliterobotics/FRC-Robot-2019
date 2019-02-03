570809aee7035fa258159051597a1f34b0ffa770
John Anselmo <jjohnanselmo@gmail.com>
2019-02-03 17:04:16 -0500

    Normal state for normality, landed for post landing

--- robot/src/main/java/us/ilite/robot/modules/EFourBarState.java
+++ robot/src/main/java/us/ilite/robot/modules/EFourBarState.java
@@ -2,11 +2,14 @@ package us.ilite.robot.modules;
 
 public enum EFourBarState {
     // TO-DO: get output
+    
+    NORMAL( 0 ),
     STOP( 0 ),
     ACCELERATE( 0 ),
     CRUISE_1( 0 ),
     CRUISE_2( 0 ),
-    DECELLERATE( 0 );
+    DECELLERATE( 0 ),
+    LANDED( 0 );
 
     private double mOutput;
     EFourBarState( double pOutput ) {
