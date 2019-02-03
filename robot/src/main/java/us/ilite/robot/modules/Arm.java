4f59f4f881fc10e2f7834d2e711963222e8b03d7
Kate <BalintKE20@pwcs-edu.org>
2019-02-02 19:57:47 -0500

    adding logic and current limiting

--- robot/src/main/java/us/ilite/robot/modules/Arm.java
+++ robot/src/main/java/us/ilite/robot/modules/Arm.java
@@ -1,45 +1,108 @@
 package us.ilite.robot.modules;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.team254.lib.drivers.talon.TalonSRXFactory;
 
+import edu.wpi.first.wpilibj.Timer;
 import us.ilite.common.config.SystemSettings;
 import us.ilite.common.lib.control.PIDController;
+import us.ilite.common.lib.control.PIDGains;
+import com.team254.lib.util.Util;
 
 public class Arm extends Module
 {
-    private double mVoltage;
-    private double mTheta;
-    private double mDesiredTheta;
-    private ESetPoint mDesiredSetPoint;
-    private ESetPoint mCurrentSetPoint;
+    private static final double MAX_CURRENT_V_RATIO = 5; //tune - overcurrent ratio for arm motor
+    // private double mVoltage;
+    // private double mActualTheta;
+    // private double mDesiredTheta;
+    // private ESetPoint mDesiredSetPoint;
+    // private ESetPoint mCurrentSetPoint;
+    private double kP;
+    private double kI;
+    private double kD;
+    private int maxNumTicks = 383; //number of ticks at max arm angle
+    private int minNumTicks = 0; //number of ticks at min arm angle
     private static int talonPortId = 0; //placeholder constant; change later in SystemSettings
-
-    // 1024 ticks/360 degrees = .351 t/d
+    private TalonSRX talon = TalonSRXFactory.createDefaultTalon(talonPortId);
+    private PIDController pid;
+    // private boolean settingPosition;
+    private int currentNumTicks = 0; //revisit this and check if correct for encoder type
+    private int desiredNumTicks = 0;
+    private double mDesiredOutput;
+    private boolean stalled = false;
+    private boolean motorOff = false; //off because of current limiting
+    private Timer mTimer = new Timer();
+    
+    // 1024 ticks/360 degrees = 2.84 ticks/degree (.351 degrees/tick)
 
     public Arm()
     {
-        TalonSRX talon = new TalonSRX(talonPortId);
+        pid = new PIDController( new PIDGains(kP, kI, kD), SystemSettings.kControlLoopPeriod );
+        pid.setInputRange( 0, 383 ); //min and max ticks of arm
     }
 
     @Override
     public void modeInit(double pNow)
     {
-        //PIDController pid = PIDController( new PIDController(kP, kI, kD), SystemSettings.kControlLoopPeriod );
-        // pid.setInputRange( min, max );
-        //
+        
     }
 
     @Override
     public void periodicInput(double pNow)
     {
-
+        
     }
 
     @Override
     public void update(double pNow)
     {
-        //mTheta = updated angle;
-        //motor output = mVoltage * cos( mTheta );
+        double current = talon.getOutputCurrent();
+        double voltage = talon.getMotorOutputVoltage();
+        double ratio = current / voltage;
+
+        if(ratio > MAX_CURRENT_V_RATIO)
+        {
+        
+            if(!stalled)
+	        {
+                mTimer.start();
+            }
+            else
+            {
+            if( mTimer.hasPeriodPassed(MAX_STALL_TIME) )
+            {
+                mDesiredOutput = 0;
+                motorOff = true;
+                mTimer.stop();
+                mTimer.start(); //starting for cool-off period
+            }
+        }
+        else
+        {
+            stalled = false;
+        }
+
+        if(motorOff)
+        {
+            if( mTimer.hasPeriodPassed(MOTOR_OFF_TIME) )
+            {
+                motorOff = false;
+                mTimer.stop();
+            }
+            else
+            {
+                mDesiredOutput = 0;
+            }
+        }
+        else //motor is not off
+        {
+            double output = this.mDesiredOutput; //= calculateOutput();
+        }
+
+        currentNumTicks = talon.getSelectedSensorPosition();
+        talon.set(ControlMode.PercentOutput, output);
+        
     }
 
     @Override
@@ -48,44 +111,67 @@ public class Arm extends Module
 
     }
 
-    public void moveByAngle( double pGoalTheta )
+    public int getSetPoints(ESetPoint pPoint)
     {
-        //motor output += returnPID( mTheta, pGoalTheta );
-    }
-
-    public double returnPID(double pTheta, double pGoal)
-    {
-        double p_gain = 0.0; //adjust
-        double d_gain = 0.0; //adjust
-        double i_gain = 0.0; //adjust
-      
-        double error = pGoal - pTheta;
-
-     
-        return 0.0;
-      }
-
-
-      public void getSetPoints( ESetPoint pPoint)
-      {
         switch( pPoint )
         {
             case FULLY_OUT:
-            //while ( !button to change position is pressed ){}
-            break;
+            //90 degrees * 2.84 ticks/degree = 256
+            return 256;
 
             case FULLY_UP:
-            //while ( !button to change position is pressed ){}
-            break;
+            //135 degrees * 2.84 ticks/degree = 383
+            return 383;
 
             case FULLY_DOWN:
-            //while ( !button to change position is pressed ){}
-            break;
+            return 0;
 
             case MANUAL_STATE:
-            //while ( !button to change position is pressed ){}
-            break;
+
         }
-      }
+        return 0;
+    }
+
+
+    private double calculateOutput()
+    {
+        pid.setSetpoint(this.desiredNumTicks);
+        double tempOutput = pid.calculate( talon.getSelectedSensorPosition(), Timer.getFPGATimestamp() );
+        return Util.limit(tempOutput, -1, 1);
+
+    }
+
+    private int angleToTicks(double angle)
+    {
+        // 1024 ticks/360 degrees = 2.84 ticks/degree (.351 degrees/tick)
+        return (int) (angle * 2.84);
+    }
+
+    private double ticksToAngle(int numTicks)
+    {
+        // 1024 ticks/360 degrees = 2.84 ticks/degree (.351 degrees/tick)
+        return numTicks * .351;
+    }
+
+    public void setArmAngle( double angle )
+    {
+        angle = Util.limit(angle, 0, 135);
+        this.desiredNumTicks = angleToTicks( angle );
+    }
+
+    public void setPoint(ESetPoint pDesiredPoint) 
+    {
+        this.desiredNumTicks = getSetPoints(pDesiredPoint);
+    }
+
+    public void setDesiredOutput( double desiredOutput )
+    {
+        this.mDesiredOutput = Util.limit(desiredOutput, -1, 1);
+    }
+
+    public double getCurrentArmAngle()
+    {
+        return ticksToAngle( this.currentNumTicks );
+    }
 
 }
--- robot/src/main/java/us/ilite/robot/modules/ESetPoint.java
+++ robot/src/main/java/us/ilite/robot/modules/ESetPoint.java
@@ -2,12 +2,10 @@ package us.ilite.robot.modules;
 
 public enum ESetPoint
 {
-  FULLY_OUT(), FULLY_UP(), FULLY_DOWN(), MANUAL_STATE(); //0 degrees(min) to 135 degrees(max)
-  
-  int mEncoderTicks;
+  FULLY_OUT, FULLY_UP, FULLY_DOWN, MANUAL_STATE; //0 degrees(min) to 135 degrees(max)
   ESetPoint()
   {
-    //this.mEncoderTicks = pEncoderTicks;
+
   }
 
 }
