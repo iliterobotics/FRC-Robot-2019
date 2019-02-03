b08675083e96fd81e7e7eb37be2c883a57923061
John Anselmo <jjohnanselmo@gmail.com>
2019-02-03 16:23:37 -0500

    Fourbar state for each section of autonomous sequence

--- robot/src/main/java/us/ilite/robot/modules/EFourBarState.java
+++ robot/src/main/java/us/ilite/robot/modules/EFourBarState.java
@@ -1,5 +1,19 @@
 package us.ilite.robot.modules;
 
 public enum EFourBarState {
-    
+    // TO-DO: get output
+    STOP( 0 ),
+    ACCELERATE( 0 ),
+    CRUISE_1( 0 ),
+    CRUISE_2( 0 ),
+    DECELLERATE( 0 );
+
+    private double mOutput;
+    EFourBarState( double pOutput ) {
+        mOutput = pOutput;
+    }
+
+    public double getOutput() {
+        return mOutput;
+    }
 }
