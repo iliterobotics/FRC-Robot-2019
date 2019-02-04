7bdd1c882903659366996d408d300d35979fa46f
John Anselmo <jjohnanselmo@gmail.com>
2019-02-04 15:55:01 -0500

    Get state from current degrees

--- robot/src/main/java/us/ilite/robot/modules/EFourBarState.java
+++ robot/src/main/java/us/ilite/robot/modules/EFourBarState.java
@@ -19,6 +19,23 @@ public enum EFourBarState {
         mLowerAngularBound = pLowerAngularBound;
         mUpperAngularBound = pUpperAngularBound;
     }
+    
+    public static EFourBarState fromDegrees( double currentDegrees ) {
+        EFourBarState[] states = { EFourBarState.NORMAL,
+                                   EFourBarState.STOP,
+                                   EFourBarState.ACCELERATE,
+                                   EFourBarState.CRUISE_1,
+                                   EFourBarState.CRUISE_2,
+                                   EFourBarState.DECELERATE,
+                                   EFourBarState.LANDED };
+        for ( EFourBarState state : states ) {
+            if ( currentDegrees >= state.getLowerAngularBound() &&
+                 currentDegrees <= state.getUpperAngularBound() ) {
+                    return state;
+            }
+        }
+        return null;
+    }
 
     public double getLowerAngularBound() {
         return mLowerAngularBound;
