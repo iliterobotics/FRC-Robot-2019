bb51dd10e074f4574ea359ffd6d2de18dd3d5321
John Anselmo <jjohnanselmo@gmail.com>
2019-02-10 09:11:14 -0500

    Start base testing for fourbar

---
+++ robot/src/test/java/us/ilite/robot/modules/FourBarTest.java
@@ -0,0 +1,19 @@
+package us.ilite.robot.modules;
+
+import static org.mockito.Mockito.*;
+import org.junit.After;
+import org.junit.Before;
+
+public class FourBarTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void teardown() {
+
+    }
+
+}
