3a88ec7ec67a6ce1fa8757b03ac6e77e242bb0b6
Stephen Welch <stephenwelchva@gmail.com>
2019-02-15 18:45:50 -0500

    Add hatch grab command

--- robot/src/main/java/us/ilite/robot/commands/GrabHatch.java
+++ robot/src/main/java/us/ilite/robot/commands/GrabHatch.java
@@ -2,27 +2,10 @@ package us.ilite.robot.commands;
 
 import us.ilite.robot.modules.HatchFlower;
 
-public class GrabHatch extends ACommand {
-
-    private final HatchFlower mHatchFlower;
+public class GrabHatch extends FunctionalCommand {
 
     public GrabHatch(HatchFlower pHatchFlower) {
-        mHatchFlower = pHatchFlower;
-    }
-
-    @Override
-    public void init(double pNow) {
-        mHatchFlower.captureHatch();
-    }
-
-    @Override
-    public boolean update(double pNow) {
-        return true;
-    }
-
-    @Override
-    public void shutdown(double pNow) {
-
+        super(pHatchFlower::captureHatch, () -> pHatchFlower.getHatchFlowerState() == HatchFlower.HatchFlowerState.CAPTURE);
     }
 
 }
