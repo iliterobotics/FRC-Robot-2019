18f6501acb00f886efcb684525f814d19a7a33be
Stephen Welch <stephenwelchva@gmail.com>
2019-02-15 18:47:45 -0500

    Add set intake position command

--- robot/src/main/java/us/ilite/robot/commands/SetIntakeState.java
+++ robot/src/main/java/us/ilite/robot/commands/SetIntakeState.java
@@ -2,29 +2,10 @@ package us.ilite.robot.commands;
 
 import us.ilite.robot.modules.Intake;
 
-public class SetIntakeState extends ACommand {
-
-    private final Intake mIntake;
-    private final Intake.EWristPosition mDesiredPosition;
+public class SetIntakeState extends FunctionalCommand {
 
     public SetIntakeState(Intake pIntake, Intake.EWristPosition pDesiredPosition) {
-        mIntake = pIntake;
-        mDesiredPosition = pDesiredPosition;
-    }
-
-    @Override
-    public void init(double pNow) {
-        mIntake.setWrist(mDesiredPosition);
-    }
-
-    @Override
-    public boolean update(double pNow) {
-        return mIntake.isAtPosition(mDesiredPosition);
-    }
-
-    @Override
-    public void shutdown(double pNow) {
-
+        super(() -> pIntake.setWrist(pDesiredPosition), () -> pIntake.isAtPosition(pDesiredPosition));
     }
 
 }
