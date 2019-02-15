029aa7ab503fa8d70e3b5ce10b09a9dad6cdcf7a
Stephen Welch <stephenwelchva@gmail.com>
2019-02-15 18:49:46 -0500

    Add intake commands

---
+++ robot/src/main/java/us/ilite/robot/commands/GroundIntakeHatch.java
@@ -0,0 +1,20 @@
+package us.ilite.robot.commands;
+
+import us.ilite.robot.modules.EElevatorPosition;
+import us.ilite.robot.modules.Elevator;
+import us.ilite.robot.modules.HatchFlower;
+import us.ilite.robot.modules.Intake;
+
+public class GroundIntakeHatch extends CommandQueue {
+
+    public GroundIntakeHatch(Elevator pElevator, Intake pIntake, HatchFlower pHatchFlower) {
+        setCommands(
+                new ParallelCommand(
+                        new SetElevatorPosition(pElevator, EElevatorPosition.BOTTOM),
+                        new ReleaseHatch(pHatchFlower),
+                        new SetHatchGrabberExtension(pHatchFlower, HatchFlower.ExtensionState.DOWN)
+                ),
+                new SetIntakeState(pIntake, Intake.EWristPosition.GROUND)
+        );
+    }
+}
---
+++ robot/src/main/java/us/ilite/robot/commands/LoadingStationIntakeHatch.java
@@ -0,0 +1,20 @@
+package us.ilite.robot.commands;
+
+import us.ilite.robot.modules.EElevatorPosition;
+import us.ilite.robot.modules.Elevator;
+import us.ilite.robot.modules.HatchFlower;
+
+public class LoadingStationIntakeHatch extends CommandQueue {
+
+    public LoadingStationIntakeHatch(Elevator pElevator, HatchFlower pHatchFlower) {
+        setCommands(
+                new ParallelCommand(
+                        new SetElevatorPosition(pElevator, EElevatorPosition.BOTTOM),
+                        new ReleaseHatch(pHatchFlower),
+                        new SetHatchGrabberExtension(pHatchFlower, HatchFlower.ExtensionState.DOWN)
+                ),
+                new GrabHatch(pHatchFlower)
+        );
+    }
+
+}
