66003c40ec11a098edc838d98770724f0454bbad
Jesse Knight <flybotix@gmail.com>
2019-02-15 22:18:53 -0500

    Only keep tuning parameters in PracticeBotSystemSettings

--- common/src/main/java/us/ilite/common/config/PracticeBotSystemSettings.java
+++ common/src/main/java/us/ilite/common/config/PracticeBotSystemSettings.java
@@ -13,44 +13,13 @@ public class PracticeBotSystemSettings {
 
     public static double NETWORK_TABLE_UPDATE_RATE = 0.01;
 
-    // ===========================
-    // System ID's
-    // ================================
-    public static int kCANAddressPCM = 20;
-
-    //==============================================================================
-    // Logging
-    // =============================================================================
-    public static String kLoggingTimestampKey = "TIME";
-
-    // =============================================================================
-    // Talon Addresses
-    // =============================================================================
-    // Encoders are on the rear Talons, so ID's are temporarily flipped around
-    public static  int kDriveLeftMasterTalonId = 1;
-    public static int kDriveLeftMiddleTalonId = 3;
-    public static  int kDriveLeftRearTalonId = 5;
-    public static  int kDriveRightMasterTalonId = 2;
-    public static int kDriveRightMiddleTalonId = 4;
-    public static  int kDriveRightRearTalonId = 6;
-
-    public static int kPigeonId = 3;
-
-    public static int kCANTimeoutMs = 10; //use for on the fly updates
-    public static int kLongCANTimeoutMs = 100; //use for constructors
 
     // =============================================================================
     // Drive Train Constants
     // =============================================================================
-    public static double kDriveClosedLoopVoltageRampRate = 0.0;
-    public static double kDriveOpenLoopVoltageRampRate = 0.1;
-    public static int kDriveCurrentLimitAmps = 40;
-    public static int kDriveCurrentLimitTriggerDurationMs = 100;
     public static double kDriveWheelDiameterInches = 6.0;
     public static double  DRIVETRAIN_WHEEL_DIAMETER_FEET = kDriveWheelDiameterInches / 12.0;
     public static double kDriveWheelCircumference = kDriveWheelDiameterInches * Math.PI;
-    public static double  DRIVETRAIN_DEFAULT_RAMP_RATE = 120.0; // in V/sec
-    public static double  DRIVETRAIN_HIGH_GEAR_RAMP_RATE = 120.0; // in V/sec
     public static double kDriveTicksPerRotation = 1024;
     public static double kDriveEffectiveWheelbase = 23.75 * 1.025;
     public static double 	DRIVETRAIN_TURN_CIRCUMFERENCE = kDriveEffectiveWheelbase * Math.PI;
@@ -58,25 +27,6 @@ public class PracticeBotSystemSettings {
     public static double	DRIVETRAIN_WHEEL_TURNS_PER_DEGREE = DRIVETRAIN_INCHES_PER_DEGREE / kDriveWheelDiameterInches;
     public static double kDriveCollisionThreshold = 0.0;
 
-    // =============================================================================
-    // Input Constants
-    // =============================================================================
-    public static double kSnailModePercentThrottleReduction = .5;
-    public static double kSnailModePercentRotateReduction = .4;
-    public static double kDriverInputTurnMaxMagnitude = 0.5;
-    public static double  INPUT_DEADBAND_F310_JOYSTICK = 0.05;
-    public static double  INPUT_DEADBAND_F310_TRIGGER = 0.5;
-    public static int     JOYSTICK_PORT_DRIVER = 0;
-    public static int     JOYSTICK_PORT_OPERATOR = 1;
-    public static int     JOYSTICK_PORT_TESTER = 2;
-
-    public static int kLimelightDefaultPipeline = ETrackingType.TARGET_LEFT.getPipeline();
-    public static List<ELogitech310> kTeleopCommandTriggers = Arrays.asList(DriveTeamInputMap.DRIVER_TRACK_TARGET_BTN, 
-                                                                            DriveTeamInputMap.DRIVER_TRACK_CARGO_BTN,
-                                                                            DriveTeamInputMap.DRIVER_TRACK_HATCH_BTN);
-    public static List<ELogitech310> kAutonOverrideTriggers = Arrays.asList(DriveTeamInputMap.DRIVER_THROTTLE_AXIS,
-                                                                            DriveTeamInputMap.DRIVER_TURN_AXIS);
-    public static double kAutonOverrideAxisThreshold = 0.3;
 
     // =============================================================================
     // Motion Magic Constants
@@ -107,7 +57,6 @@ public class PracticeBotSystemSettings {
     public static double kElevatorI = 0; 
     public static double kElevatorD = 0;
     public static double kElevatorF = 0;
-    public static double kELevatorControlLoopPeriod = 0.01;
     public static double kElevatorMinPower = -1.0;
     public static double kElevatorMaxPower = 1.0;
     public static int kElevatorCurrentLimit = 10;
@@ -152,56 +101,6 @@ public class PracticeBotSystemSettings {
     public static double llRightACoeff = -54.3943883842204;
     public static double llRightBCoeff = -4.53956454545558;
     public static double llRightCCoeff = -0.0437470770400814;
-    // =============================================================================
-    // Hatch Flower constants
-    // =============================================================================
-    public static double kHatchFlowerSolenoidReleaseTimeSec = 0.250;
-    public static double kHatchFlowerPushDurationSec = 0.250;
-
-    // kHatchFlowerGrabToPushTransitionTimeSec is the time between releasing the 
-    // grab solenoid and engaging the push solenoid.
-    public static double kHatchFlowerGrabToPushTransitionTimeSec = 0.250;
-
-
-    // =============================================================================
-    // 2019 Module Addresses
-    // =============================================================================
-    public static int kCargoSpitLeftSPXAddress = 13;
-    public static int kCargoSpitRightSPXAddress = 14;
-    public static double kCargoSpitSPXCurrentLimit = -1.0;
-
-    public static int kElevatorNEOAddress = -15;
-    // TO-DO: Elevator encoder address?
-    public static int kElevatorNEOEncoderAddress = -1;
-    // public static int kElevatorRedundantEncoderAddress = -1;
-
-    public static int kFourBarNEO1Address = 9;
-    public static int kFourBarNEO2Address = 10;
-    // TO-DO: label solenoid as forward/reverse in spreadsheet
-    public static int kFourBarDoubleSolenoidForwardAddress = 0;
-    public static int kFourBarDoubleSolenoidReverseAddress = 1;
-    public static int kFourBarTBDSensorAddress = -1;
-
-    public static int kHatchFlowerOpenCloseSolenoidAddress = 5;
-    public static int kHatchFlowerExtensionSolenoidAddress = 6;
-
-    public static int kHatchIntakeSPXAddress = 11;
-    public static int kCargoIntakeSPXLowerAddress = 12;
-    // TO-DO DIO spreadsheet empty
-    public static int kIntakeBeamBreakAddress = -1;
-
-    public static int kIntakeWristSRXAddress = 16;
-    // TO-DO Writs encoder addresses?
-    public static int kIntakeWristEncoderA_Address = -1;
-    public static int kIntakeWristEncoderB_Address = -1;
-    public static double kIntakeWristCurrentLimit = -1.0;
-
-    public static int kDriveTrainRightSRX1Address = 2;
-    public static int kDriveTrainRightSPX2Address = 4;
-    public static int kDriveTrainRightSPX3Address = 6;
-    public static int kDriveTrainLeftSRX1Address = 1;
-    public static int kDriveTrainLeftSPX2Address = 3;
-    public static int kDriveTrainLeftSPX3Address = 5;
 
     public static PracticeBotSystemSettings getInstance() {
         return INSTANCE_HOLDER.sInstance;
