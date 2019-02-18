29748aed5591497f1e781ae39e602db1b5786ba6
Stephen Welch <stephenwelchva@gmail.com>
2019-02-18 11:04:51 -0500

    Comment out arm control code from testing

--- robot/src/main/java/us/ilite/robot/driverinput/DriverInput.java
+++ robot/src/main/java/us/ilite/robot/driverinput/DriverInput.java
@@ -114,7 +114,7 @@ public class DriverInput extends Module implements IThrottleProvider, ITurnProvi
         if (!mAutonomousCommandManager.isRunningCommands()) {
             updateDriveTrain();
             updateElevator();
-            updateArm();
+//            updateArm();
             updateIntake();
         } 
 
@@ -208,6 +208,7 @@ public class DriverInput extends Module implements IThrottleProvider, ITurnProvi
         // }
 
         DriveMessage driveMessage = DriveMessage.fromThrottleAndTurn(throttle, rotate);
+
         driveMessage.setNeutralMode(NeutralMode.Brake);
         driveMessage.setControlMode(ControlMode.PercentOutput);
 
@@ -215,53 +216,53 @@ public class DriverInput extends Module implements IThrottleProvider, ITurnProvi
 
     }
 
-    /**
-     * Commands the superstructure to update where the arm should move 
-     * depending on joystick movements. (in progress)
-     */
-    protected void updateArm()
-    {
-        double mult = 1.0;
-        //temporarily assuming this setpoint will be set by the operator Y button
-        if( mOperatorInputCodex.isSet( DriveTeamInputMap.OPERATOR_ARM_SETPOINT_UP ) )
-        {
-            mArm.setArmAngle(SystemSettings.ArmPosition.FULLY_UP.getAngle());
-        }
-        //temporarily assuming this setpoint will be set by the operator A button
-        else if( mOperatorInputCodex.isSet( DriveTeamInputMap.OPERATOR_ARM_SETPOINT_DOWN ) )
-        {
-            mArm.setArmAngle(SystemSettings.ArmPosition.FULLY_DOWN.getAngle());
-        }
-        //temporarily assuming this setpoint will be set by the operator B button
-        else if( mOperatorInputCodex.isSet( DriveTeamInputMap.OPERATOR_ARM_SETPOINT_OUT ) )
-        {
-            mArm.setArmAngle(SystemSettings.ArmPosition.FULLY_OUT.getAngle());
-        }
-        //temporarily assuming the arm will be controlled by the operator joystick
-        else if( mOperatorInputCodex.isSet( DriveTeamInputMap.OPERATOR_ARM_MOTION ) )
-        {
-            //mArm.setArmAngle( mArm.getCurrentArmAngle() + mOperatorInputCodex.get( DriveTeamInputMap.OPERATOR_ARM_MOTION ) );
-            // System.out.println(mOperatorInputCodex.get( DriveTeamInputMap.OPERATOR_ARM_MOTION ));
-
-            // // Drive the arm directly with the joystick.  Joystick output is -1 to 1
-            // // Talon desired output range is -1 to 1
-            // // Scale the output by the button pressed
-            // // which of these is correct???  both?
-            // mArm.setDesiredOutput( mOperatorInputCodex.get( DriveTeamInputMap.OPERATOR_ARM_MOTION ) * mult );
-            // mArm.setDesiredOutput( mData.operatorinput.get( DriveTeamInputMap.OPERATOR_ARM_MOTION ) * mult );
-
-            // System.out.println( "+++++++++++++++DriverInput operator joystick: " + mData.operatorinput.get( DriveTeamInputMap.OPERATOR_ARM_MOTION ));
-
-            // Drive the arm to track the joystick
-            // Assuming a mapping of 0 to 135 deg for the joysticks -1 to 1
-            // angle = ((joystick + 1)/2) * 135
-            // double angle = (mData.operatorinput.get( DriveTeamInputMap.OPERATOR_ARM_MOTION ) + 1 ) / 2 * 135;
-            
-            double angle = this.armJoyStickToAngleScaler.scaleAtoB(mData.operatorinput.get( DriveTeamInputMap.OPERATOR_ARM_MOTION ));
-            mArm.setArmAngle(angle);
-
-        }
-    }
+//    /**
+//     * Commands the superstructure to update where the arm should move
+//     * depending on joystick movements. (in progress)
+//     */
+//    protected void updateArm()
+//    {
+//        double mult = 1.0;
+//        //temporarily assuming this setpoint will be set by the operator Y button
+//        if( mOperatorInputCodex.isSet( DriveTeamInputMap.OPERATOR_ARM_SETPOINT_UP ) )
+//        {
+//            mArm.setArmAngle(SystemSettings.ArmPosition.FULLY_UP.getAngle());
+//        }
+//        //temporarily assuming this setpoint will be set by the operator A button
+//        else if( mOperatorInputCodex.isSet( DriveTeamInputMap.OPERATOR_ARM_SETPOINT_DOWN ) )
+//        {
+//            mArm.setArmAngle(SystemSettings.ArmPosition.FULLY_DOWN.getAngle());
+//        }
+//        //temporarily assuming this setpoint will be set by the operator B button
+//        else if( mOperatorInputCodex.isSet( DriveTeamInputMap.OPERATOR_ARM_SETPOINT_OUT ) )
+//        {
+//            mArm.setArmAngle(SystemSettings.ArmPosition.FULLY_OUT.getAngle());
+//        }
+//        //temporarily assuming the arm will be controlled by the operator joystick
+//        else if( mOperatorInputCodex.isSet( DriveTeamInputMap.OPERATOR_ARM_MOTION ) )
+//        {
+//            //mArm.setArmAngle( mArm.getCurrentArmAngle() + mOperatorInputCodex.get( DriveTeamInputMap.OPERATOR_ARM_MOTION ) );
+//            // System.out.println(mOperatorInputCodex.get( DriveTeamInputMap.OPERATOR_ARM_MOTION ));
+//
+//            // // Drive the arm directly with the joystick.  Joystick output is -1 to 1
+//            // // Talon desired output range is -1 to 1
+//            // // Scale the output by the button pressed
+//            // // which of these is correct???  both?
+//            // mArm.setDesiredOutput( mOperatorInputCodex.get( DriveTeamInputMap.OPERATOR_ARM_MOTION ) * mult );
+//            // mArm.setDesiredOutput( mData.operatorinput.get( DriveTeamInputMap.OPERATOR_ARM_MOTION ) * mult );
+//
+//            // System.out.println( "+++++++++++++++DriverInput operator joystick: " + mData.operatorinput.get( DriveTeamInputMap.OPERATOR_ARM_MOTION ));
+//
+//            // Drive the arm to track the joystick
+//            // Assuming a mapping of 0 to 135 deg for the joysticks -1 to 1
+//            // angle = ((joystick + 1)/2) * 135
+//            // double angle = (mData.operatorinput.get( DriveTeamInputMap.OPERATOR_ARM_MOTION ) + 1 ) / 2 * 135;
+//
+//            double angle = this.armJoyStickToAngleScaler.scaleAtoB(mData.operatorinput.get( DriveTeamInputMap.OPERATOR_ARM_MOTION ));
+//            mArm.setArmAngle(angle);
+//
+//        }
+//    }
 
     /**
      * Commands the superstructure to start vision tracking depending on
