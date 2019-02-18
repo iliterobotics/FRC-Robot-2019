f5f07669bced927154ef87e718167f3f742c07a6
Stephen Welch <stephenwelchva@gmail.com>
2019-02-17 20:09:57 -0500

    Add wrapper command for limelight

--- robot/src/main/java/us/ilite/robot/commands/LimelightTargetLock.java
+++ robot/src/main/java/us/ilite/robot/commands/LimelightTargetLock.java
@@ -1,4 +1,26 @@
 package us.ilite.robot.commands;
 
-public class LimelightTargetLock {
+import us.ilite.common.types.ETrackingType;
+import us.ilite.robot.modules.Drive;
+import us.ilite.robot.modules.IThrottleProvider;
+import us.ilite.robot.modules.Limelight;
+import us.ilite.robot.modules.targetData.ITargetDataProvider;
+
+public class LimelightTargetLock extends TargetLock {
+
+    private Limelight mLimelight;
+
+    public LimelightTargetLock(Drive pDrive, Limelight pLimelight, double pAllowableError, ETrackingType pTrackingType, ITargetDataProvider pCamera, IThrottleProvider pThrottleProvider) {
+        super(pDrive, pAllowableError, pTrackingType, pCamera, pThrottleProvider);
+        this.mLimelight = pLimelight;
+        mLimelight.setPipeline(pTrackingType.getPipeline());
+    }
+
+    public LimelightTargetLock(Drive pDrive, Limelight pLimelight, double pAllowableError, ETrackingType pTrackingType, ITargetDataProvider pCamera, IThrottleProvider pThrottleProvider, boolean pEndOnAlignment) {
+        super(pDrive, pAllowableError, pTrackingType, pCamera, pThrottleProvider, pEndOnAlignment);
+
+        this.mLimelight = pLimelight;
+        mLimelight.setPipeline(pTrackingType.getPipeline());
+    }
+
 }
