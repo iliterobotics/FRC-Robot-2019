01e210bede8ec9783075f3a406cbd1daa48f251e
Stephen Welch <stephenwelchva@gmail.com>
2019-02-19 21:54:51 -0500

    Change Victor SPX to factory

--- robot/src/main/java/us/ilite/robot/modules/CargoSpit.java
+++ robot/src/main/java/us/ilite/robot/modules/CargoSpit.java
@@ -5,8 +5,8 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.flybotix.hfr.util.log.ILog;
 import com.flybotix.hfr.util.log.Logger;
-import com.team254.lib.drivers.TalonSRXFactory;
 
+import com.team254.lib.drivers.talon.TalonSRXFactory;
 import us.ilite.common.Data;
 import us.ilite.common.config.SystemSettings;
 import us.ilite.common.types.manipulator.ECargoSpit;
@@ -34,8 +34,10 @@ public class CargoSpit extends Module {
 
         this.mData = pData;
         // TODO Change to VictorSPX (or keep as TalonSRX)
-        mLeftMotor = new VictorSPX(SystemSettings.kCargoSpitLeftSPXAddress);
-        mRightMotor = new VictorSPX(SystemSettings.kCargoSpitRightSPXAddress);
+        mLeftMotor = TalonSRXFactory.createDefaultVictor(SystemSettings.kCargoSpitLeftSPXAddress);
+//        mLeftMotor = new VictorSPX(SystemSettings.kCargoSpitLeftSPXAddress);
+        mRightMotor = TalonSRXFactory.createDefaultVictor(SystemSettings.kCargoSpitRightSPXAddress);
+//        mRightMotor = new VictorSPX(SystemSettings.kCargoSpitRightSPXAddress);
         //TODO figure out these values and make them constants
         mRightMotor.configOpenloopRamp( mPower, 5 );
         mLeftMotor.configOpenloopRamp( mPower, 5 );
