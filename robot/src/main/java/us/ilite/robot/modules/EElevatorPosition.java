29ba1162d5ae754973a492a33517f3af02552844
Daniel <danielchristopher19@gmail.com>
2019-02-23 17:54:51 -0500

    Motion Magic Tuned, Setpoints created

--- robot/src/main/java/us/ilite/robot/modules/EElevatorPosition.java
+++ robot/src/main/java/us/ilite/robot/modules/EElevatorPosition.java
@@ -3,24 +3,18 @@ package us.ilite.robot.modules;
 public enum EElevatorPosition {
 
     //TODO find encoder threshold
-    HATCH_BOTTOM(0.1d,500),
-    HATCH_MIDDLE(0.1d,1000),
-    HATCH_TOP(0.1d,1500),
-    CARGO_BOTTOM(0.1d,500),
-    CARGO_MIDDLE(0.1d,1000),
-    CARGO_TOP(0.1d,1500);
+    HATCH_BOTTOM(0),
+    HATCH_MIDDLE(16),
+    HATCH_TOP(34),
+    CARGO_BOTTOM(8.5),
+    CARGO_MIDDLE(25),
+    CARGO_TOP(41.5);
 
     private double kEncoderThreshold;
-    private final double kSetPointPower;
 
-    EElevatorPosition( double pPower, int pEncoderThreshold ) {
-        this.kSetPointPower = pPower;
+    EElevatorPosition( double pEncoderThreshold ) {
         this.kEncoderThreshold = pEncoderThreshold;
 
-    }
-
-    public double getSetPointPower() {
-        return kSetPointPower;
     }
 
     public double mEncoderThreshold() {
