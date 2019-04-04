ef66afaa1217dd05cff88074ffd11b49fc06d686
John Anselmo <jjohnanselmo@gmail.com>
2019-04-04 19:05:50 -0400

    Handle nullpointer in matchmetadata

--- common/src/main/java/us/ilite/common/io/CodexCsvLogger.java
+++ common/src/main/java/us/ilite/common/io/CodexCsvLogger.java
@@ -12,6 +12,8 @@ import com.flybotix.hfr.codex.Codex;
 
 import com.flybotix.hfr.util.log.ILog;
 import com.flybotix.hfr.util.log.Logger;
+
+import edu.wpi.first.wpilibj.DriverStation;
 import us.ilite.common.Data;
 import us.ilite.common.types.MatchMetadata;
 
@@ -40,22 +42,26 @@ public class CodexCsvLogger {
 
     }
 
-    public void writeHeader() {
+    public boolean writeHeader() {
         try {
             writer.append(mCodex.getCSVHeader());
             writer.newLine();
         } catch (IOException pE) {
             pE.printStackTrace();
+            return false;
         }
+        return true;
     }
 
-    public void writeLine() {
+    public boolean writeLine() {
         try {
             writer.append(mCodex.toCSV());
             writer.newLine();
         } catch (IOException pE) {
             pE.printStackTrace();
+            return false;
         }
+        return true;
     }
 
     public File file(boolean handleUSBConnection) {
@@ -70,18 +76,27 @@ public class CodexCsvLogger {
         // } else {
         //     dir = USER_DIR;
         // }
-
-        String eventName = mMatchData.mEventName;
-        if ( eventName.length() <= 0 ) {
-            // event name format: MM-DD-YYYY_HH-MM-SS
-            eventName =  new SimpleDateFormat("MM-dd-YYYY_HH-mm-ss").format(Calendar.getInstance().getTime());
+        String mEventName;
+        String mMatchType;
+        Integer mMatchNumber;
+        if(mMatchData != null) {
+            mEventName = mMatchData.mEventName;
+            mMatchType = mMatchData.mMatchType.toString();
+            mMatchNumber = mMatchData.mMatchNumber;
+        } else {
+            mEventName = DriverStation.getInstance().getEventName();
+            mMatchType = DriverStation.getInstance().getMatchType().toString();
+            mMatchNumber = DriverStation.getInstance().getMatchNumber();
         }
-
+        if ( mEventName.length() <= 0 ) {
+            // event name format: MM-DD-YYYY_HH-MM-SS
+            mEventName =  new SimpleDateFormat("MM-dd-YYYY_HH-mm-ss").format(Calendar.getInstance().getTime());
+        }
         File file = new File(String.format( dir + LOG_PATH_FORMAT,
                             mCodex.meta().getEnum().getSimpleName(),
-                            eventName,
-                            mMatchData.mMatchType,
-                            Integer.toString(mMatchData.mMatchNumber)
+                            mEventName,
+                            mMatchType,
+                            Integer.toString(mMatchNumber)
                             ));
 
         mLog.error("Creating log file at ", file.toPath());
