d39cd3c8583bdf9c1a17790aa1444b28ef20e6fd
John Anselmo <jjohnanselmo@gmail.com>
2019-04-09 20:08:06 -0400

    start logging cleanup

--- robot/src/main/java/us/ilite/robot/Robot.java
+++ robot/src/main/java/us/ilite/robot/Robot.java
@@ -146,7 +146,7 @@ public class Robot extends TimedRobot {
 //        mAutonomousCommandManager.startCommands(new CharacterizeDrive(mDrive, false, true));
 
         mData.registerCodices();
-       mCSVLogger.start(); // Start csv logging
+        mCSVLogger.start(); // Start csv logging
 
         initTimer.stop();
         mLogger.info("Autonomous initialization finished. Took: ", initTimer.get(), " seconds");
@@ -170,7 +170,7 @@ public class Robot extends TimedRobot {
         mLoopManager.setRunningLoops(mLimelight, mDrive);
         mLoopManager.start();
 
-       mCSVLogger.start(); // start csv logging
+        mCSVLogger.start(); // start csv logging
     }
 
     @Override
