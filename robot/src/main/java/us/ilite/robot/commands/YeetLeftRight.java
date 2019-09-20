50ebe2e9daa0e6f8bfd6ac0aa87b29c76e01dbce
John Anselmo <jjohnanselmo@gmail.com>
2019-09-19 20:37:30 -0400

    Add in additional structure for command configuration

--- robot/src/main/java/us/ilite/robot/commands/YeetLeftRight.java
+++ robot/src/main/java/us/ilite/robot/commands/YeetLeftRight.java
@@ -1,14 +1,31 @@
 package us.ilite.robot.commands;
 
+import us.ilite.common.Data;
+import us.ilite.common.types.manipulator.EElevator;
+import us.ilite.lib.drivers.ECommonControlMode;
 import us.ilite.robot.modules.Drive;
 import us.ilite.robot.modules.DriveMessage;
 
 public class YeetLeftRight implements ICommand {
 
+    private Data mData;
     private Drive mDrive;
 
-    public YeetLeftRight(Drive pDrive) {
-        mDrive = pDrive;
+    private double mElevatorTicks;
+    private double mVelocityRampRate;
+    private double mCruiseVelocity;
+
+    private YeetSide mSideToTurn;
+
+    public enum YeetSide {
+        LEFT,
+        RIGHT
+    }
+
+    public YeetLeftRight(Data pData, Drive pDrive) {
+        this.mData = pData;
+        this.mDrive = pDrive;
+        mElevatorTicks = mData.elevator.get(EElevator.CURRENT_ENCODER_TICKS);
     }
 
     @Override
@@ -19,7 +36,29 @@ public class YeetLeftRight implements ICommand {
     @Override
     public boolean update(double pNow) {
 
+        mElevatorTicks = mData.elevator.get(EElevator.CURRENT_ENCODER_TICKS);
+
+        switch(mSideToTurn) {
+            case LEFT:
+                mDrive.setDriveMessage(new DriveMessage(0, 0.0, ECommonControlMode.PERCENT_OUTPUT)); // Keep right 0.0
+
+            case RIGHT:
+                mDrive.setDriveMessage(new DriveMessage(0.0, 0, ECommonControlMode.PERCENT_OUTPUT)); // Keep left 0.0
+        }
+
         return false;
+    }
+
+    /**
+     * Given the current position of the elevator, return the maximum output
+     * @return max output given elevator ticks
+     */
+    public double getMaxOutput() {
+        return 0.0;
+    }
+
+    public void setRampRate() {
+
     }
 
     @Override
