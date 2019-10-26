b34ecf89899febe66660e9337260422204e66a74
SilentWanderer <swordsnclaws@verizon.net>
2019-10-26 16:10:46 -0400

    Lower ff more

--- robot/src/main/java/us/ilite/robot/modules/Drive.java
+++ robot/src/main/java/us/ilite/robot/modules/Drive.java
@@ -240,7 +240,7 @@ public class Drive extends Loop {
 					//if there is a target in the limelight's fov, lock onto target using feedback loop
 					pidOutput = mTargetAngleLockPid.calculate(-1.0 * targetData.get(ETargetingData.tx), pNow - mPreviousTime);
 
-					pidOutput = pidOutput + (Math.signum(pidOutput) * SystemSettings.kTargetAngleLockFrictionFeedforward * .95); // Lowering friction feed forward for omnis
+					pidOutput = pidOutput + (Math.signum(pidOutput) * SystemSettings.kTargetAngleLockFrictionFeedforward * .92); // Lowering friction feed forward for omnis
 
 					mDriveMessage = DriveMessage.getClampedTurnDrive(mTargetTrackingThrottle, pidOutput);
 					// If we've already seen the target and lose tracking, exit.
